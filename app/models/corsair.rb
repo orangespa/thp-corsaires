c47bf35674c50ad1063192969b80e1f6fefa5bb8
hong <luu.bichhong@gmail.com>
2017-07-28 17:25:52 +0200

    finish all

--- app/models/corsair.rb
+++ app/models/corsair.rb
@@ -1,6 +1,6 @@
 class Corsair < ApplicationRecord
   validates :first_name, length: {maximum: 20}
-  validates :age, :inclusion => {:in => 1..6.5}
+  validates :age, :inclusion =>{:in => (15..120)}
   validates :slack_handle, format: { without: /\s/ }
   validates :github_handle, format: { without: /\s/ }
 end
